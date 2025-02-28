0512f62bb9df219eb1eec26111d334296fa8bbfc
artxta <artxta@ORDENADOR>
2025-02-28 23:55:55 +0100

    Se ha añadido documentación JavaDoc de los metodos y clases

--- src/cuentas/clientes/Cuenta.java
+++ src/cuentas/clientes/Cuenta.java
@@ -1,5 +1,15 @@
+/**
+ * clase donde estan los constructores y sus metodos
+ */
 package cuentas.clientes;
 
+
+/**
+ * 
+ * @author Jose Alberto Torresano
+ * @version 1.0
+ * @since 28-Feb-2025
+ */
 public class Cuenta {
 
 
@@ -8,6 +18,9 @@ public class Cuenta {
     private double saldo;
     private double tipoInteres;
 
+    /**
+     * constructor por defecto de la clase Cuenta sin parametros
+     */
     public Cuenta()
     {
         nombre ="";
@@ -16,6 +29,13 @@ public class Cuenta {
         tipoInteres=0;
     }
 
+    /**
+     * constructor de la clase cuenta con parametros
+     * @param nom indica nombre cliente tipo String
+     * @param cue indica el numero de cuenta tipo String
+     * @param sal indica el saldo de la cuenta
+     * @param tipo indica el tipo de interes de la cuenta
+     */
     public Cuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -24,11 +44,20 @@ public class Cuenta {
         tipoInteres=tipo;
     }
 
+    /**
+     * devuelve el saldo de la cuenta
+     * @return devuelve el saldo de la cuenta
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * ingresar ingresa dinero en la cuenta
+     * @param cantidad cantidad a ingresar en la cuenta
+     * @throws Exception si se intenta ingresar una cantidad negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -36,6 +65,12 @@ public class Cuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * retira dinero de la cuenta
+     * @param cantidad la cantidad a retirar 
+     * @throws Exception si se intenta retirar una cantidad negativa
+     *  Exception si el saldo que se quiere retirar es más alto que el saldo en la cuenta
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -45,34 +80,67 @@ public class Cuenta {
         saldo = saldo - cantidad;
     }
 
+    /**
+     * devuelve el nombre del usuario
+     * @return 
+     */
     public String getNombre() {
         return nombre;
     }
+    
 
+    /**
+     * setea el nombre del usuario
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * devuelve el numero de la cuenta
+     * @return 
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * setea el numero de la cuenta
+     * @param cuenta 
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * devuelve el saldo en la cuenta
+     * @return 
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * fija el saldo en la cuenta
+     * @param saldo 
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     * devuelve el tipo de interes de la cuenta
+     * @return 
+     */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
+    /**
+     * fija el tipo de interes en la cuenta
+     * @param tipoInteres 
+     */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
--- src/cuentas/clientes/Main.java
+++ src/cuentas/clientes/Main.java
@@ -1,11 +1,19 @@
+/**
+ * clase relacionadas con clientes y cuentas bancarias.
+ */
 package cuentas.clientes;
 
 public class Main {
 
     /*
     Tarea de Entornos de Desarrollo
-    José Alberto Torresano
     */
+    
+    /**
+     * @author José Alberto Torresano
+     * @version 1.0
+     * @since 28-Feb-2025
+     */
     public static void main(String[] args) {
         
         
@@ -15,6 +23,12 @@ public class Main {
         operativa_cuenta(0);
     }
 
+    /**
+     * este metodo crea unas operaciones en una cuenta
+     * @version 1.0
+     * @param cantidad este parametro no es usado
+     * 
+     */
     private static void operativa_cuenta(float cantidad) {
         Cuenta cuenta1;
         double saldoActual;
